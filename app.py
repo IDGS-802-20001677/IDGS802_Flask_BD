61b4826d5f7ef2a3bbdb262a543314718c5e364a
IDGS-802-20001677 <20001677@alumnos.utleon.edu.mx>
2023-03-11 10:59:12 -0600

    11-3-23 crud alumnos

--- app.py
+++ app.py
@@ -22,11 +22,19 @@ def index():
                         )
         db.session.add(alumn)
         db.session.commit()
+        return redirect(url_for('ABCompleto'))
     return render_template("index.html", form = create_form)
+
+@app.route('/ABCompleto', methods=['GET', 'POST'])
+def ABCompleto():
+    create_form = forms.UseForm(request.form)
+    #select * from alumnos
+    alumnos = Alumnos.query.all()
+    return render_template('ABCompleto.html', form = create_form, alumnos = alumnos)
 
 @app.route("/modificar", methods=["GET", "POST"])
 def modificar():
-    create_form = forms.UseForms(request.form)
+    create_form = forms.UseForm(request.form)
     if request.method == 'GET':
         id = request.args.get('id')
         alum1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
@@ -44,9 +52,26 @@ def modificar():
         db.session.commit()
 
         return redirect(url_for('ABCompleto'))
-    return render_template("modoficar.html", form = create_form)
+    return render_template("modificar.html", form = create_form)
     
-
+@app.route('/eliminar', methods=['GET', 'POST'])
+def eliminar():
+    create_form = forms.UseForm(request.form)
+    if request.method == 'GET':
+        id = request.args.get('id')
+        # select * from alumnos where id = id
+        alumno =db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = id
+        create_form.nombre.data = alumno.nombre
+        create_form.apellidos.data = alumno.apellidos
+        create_form.email.data = alumno.email
+    if request.method == 'POST':
+        id = create_form.id.data
+        alumno = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        db.session.delete(alumno)
+        db.session.commit()
+        return redirect(url_for('ABCompleto'))
+    return render_template('eliminar.html', form = create_form)
 
 if __name__ =="__main__":
     crsf.init_app(app)
